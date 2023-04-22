20a0f9197b16a851a2aff4fb3ecbd5fef5a6c512
MeenakshiGi <meena.malipatil@gmail.com>
2023-04-22 08:34:58 +0530

    Commit revision demo pjt

---
+++ src/test/java/TestClassForGitRevision.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestClassForGitRevision {
+	@Test
+	public void demo()
+	{
+		System.out.println("Hi");
+		
+		System.out.println("Hello");
+
+		System.out.println("Hello-------------");
+
+	}
+}
